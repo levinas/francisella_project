55d183dd25f88b338de961c32876810b1aaa5712
Fangfang Xia <fangfang@mcs.anl.gov>
2014-07-17 18:34:30 -0500

    add Broad assemblies

---
+++ olive/Broad_Olive_Data_Use_Policy.md
@@ -0,0 +1,40 @@
+Scaffold and gene FASTA files downloaded from:
+
+https://olive.broadinstitute.org/collections/francisella_tularensis_schu_s4.1/strains
+
+
+Data Use Policy
+===============
+
+Our goal is to make sequence data rapidly and broadly available to the
+scientific community as a community resource. It is our intention to
+publish the work of this project in a timely fashion, and we welcome
+collaborative interaction on the project and analyses.
+
+However, considerable investment was made in generating these data and
+we ask that you respect rights of first publication and acknowledgment
+as outlined in the Toronto agreement [Toronto International Data
+Release Workshop Authors. Prepublication data sharing. Nature. 2009
+Sep 10;461(7261):168-70].
+
+By accessing these data, you agree not to publish any articles
+containing analyses of genes or genomic data on a whole genome or
+chromosome scale prior to initial publication by the Broad Institute
+and its collaborating scientists.
+
+If you wish to make use of restricted data for publication or are
+interested in collaborating on the analyses of these data, please
+contact the project scientist through the link below.
+
+Any redistribution of these data should include the full text of the
+data use policy.
+
+Initiative
+Francisella tularensis
+
+Project Scientist
+Bruce Birren
+
+Status
+Data Use Policy in Effect
+
---
+++ olive/download.sh
@@ -0,0 +1,22 @@
+
+
+curl https://olive.broadinstitute.org/collections/francisella_tularensis_schu_s4_fsc043.1/downloads/scaffolds.fasta > FSC043.scaffolds.fasta
+curl https://olive.broadinstitute.org/collections/francisella_tularensis_schu_s4_fts_634_635.1/downloads/scaffolds.fasta > FTS-634.scaffolds.fasta
+curl https://olive.broadinstitute.org/strains/francisella_tularensis_schu_s4_nr_10492.1/downloads/scaffolds.fasta > NR-10492.scaffolds.fasta
+curl https://olive.broadinstitute.org/strains/francisella_tularensis_schu_s4_nr_28534.1/downloads/scaffolds.fasta > NR-28534.scaffolds.fasta
+curl https://olive.broadinstitute.org/strains/francisella_tularensis_schu_s4_nr_643.1/downloads/scaffolds.fasta > NR-643.scaffolds.fasta
+curl https://olive.broadinstitute.org/strains/francisella_tularensis_schu_s4_sl.1/downloads/scaffolds.fasta > SL.scaffolds.fasta
+
+curl https://olive.broadinstitute.org/collections/francisella_tularensis_schu_s4_fsc043.1/downloads/genes.gtf > FSC043.genes.gtf
+curl https://olive.broadinstitute.org/collections/francisella_tularensis_schu_s4_fts_634_635.1/downloads/genes.gtf > FTS-634.genes.gtf
+curl https://olive.broadinstitute.org/strains/francisella_tularensis_schu_s4_nr_10492.1/downloads/genes.gtf > NR-10492.genes.gtf
+curl https://olive.broadinstitute.org/strains/francisella_tularensis_schu_s4_nr_28534.1/downloads/genes.gtf > NR-28534.genes.gtf
+curl https://olive.broadinstitute.org/strains/francisella_tularensis_schu_s4_nr_643.1/downloads/genes.gtf > NR-643.genes.gtf
+curl https://olive.broadinstitute.org/strains/francisella_tularensis_schu_s4_sl.1/downloads/genes.gtf > SL.genes.gtf
+
+curl https://olive.broadinstitute.org/collections/francisella_tularensis_schu_s4_fsc043.1/downloads/genes.fasta > FSC043.genes.fasta
+curl https://olive.broadinstitute.org/collections/francisella_tularensis_schu_s4_fts_634_635.1/downloads/genes.fasta > FTS-634.genes.fasta
+curl https://olive.broadinstitute.org/strains/francisella_tularensis_schu_s4_nr_10492.1/downloads/genes.fasta > NR-10492.genes.fasta
+curl https://olive.broadinstitute.org/strains/francisella_tularensis_schu_s4_nr_28534.1/downloads/genes.fasta > NR-28534.genes.fasta
+curl https://olive.broadinstitute.org/strains/francisella_tularensis_schu_s4_nr_643.1/downloads/genes.fasta > NR-643.genes.fasta
+curl https://olive.broadinstitute.org/strains/francisella_tularensis_schu_s4_sl.1/downloads/genes.fasta > SL.genes.fasta
